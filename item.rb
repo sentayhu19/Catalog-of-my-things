67cb40aa2956e47637f8d2cfab6e14d9250f6993
moise <moisemlg90@gmail.com>
2022-06-28 11:57:11 +0200

    Fix parameters metric lenght

--- item.rb
+++ item.rb
@@ -1,14 +1,13 @@
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(genre, author, source, label, publish_date, archived)
-    super(genre, author, source, label, publish_date, archived)
+  def initialize(genre, author, source, label, publish_date)
     @genre = genre
     @author = author
     @source = source
     @label = label
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
 
   def can_be_archived?
