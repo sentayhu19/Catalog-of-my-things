d096b5e1fc1c2e207ecbbefb06b76fdf4f059a4c
moise <moisemlg90@gmail.com>
2022-06-28 21:43:05 +0200

    Add add_method in genre class

--- catalog_classes/genre.rb
+++ catalog_classes/genre.rb
@@ -2,12 +2,15 @@ require './item'
 
 class Genre < Item
   attr_accessor :name, :items
+  attr_reader :item
 
   def initialize(name)
     super(genre, author, source, label, publish_date)
     @name = name
-    @items = []
+    @item = []
   end
 
-  def add_item; end
+  def add_item(enre, author, source, label, publish_date)
+    Item.new(genre, author, source, label, publish_date, self)
+  end
 end
