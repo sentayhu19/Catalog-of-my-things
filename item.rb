6ab312f02e4f207b836518b44d5e20ee890fa934
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-28 12:45:05 +0300

    Linters error fixed

--- item.rb
+++ item.rb
@@ -1,19 +1,21 @@
 class Item
-    attr_accessor :genre, :author, :source, :label, :publish_date, :archived
-    def initialize(genre, author, source, label, publish_date, archived)
-        super(genre, author, source, label, publish_date, archived)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
-    def can_be_archived?
-        published_date>10
-    end
-    def move_to_archive
-        if can_be_archived
-            @archived=true
-    end
+  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(genre, author, source, label, publish_date, archived)
+    super(genre, author, source, label, publish_date, archived)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?
+    published_date > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived
+  end
 end
