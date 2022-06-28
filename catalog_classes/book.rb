37fd0fd1dc0e5f4f53dbb092260faa1d94fb7486
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-28 16:55:20 +0300

    Lable and Book class with properties created

---
+++ catalog_classes/book.rb
@@ -0,0 +1,8 @@
+class Book
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?; end
+end
---
+++ catalog_classes/lable.rb
@@ -0,0 +1,8 @@
+class Label
+  def initialize(title, color, _items)
+    @title = title
+    @color = color
+  end
+
+  def add_item(item); end
+end
