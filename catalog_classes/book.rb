7a626ee56baa1f89f141fd34cd3500767d4f57c3
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-29 17:38:37 +0300

    methods for lable and book created

--- catalog_classes/book.rb
+++ catalog_classes/book.rb
@@ -1,8 +1,12 @@
 class Book
+  attr_accessor :cover_state, :publisher
+
   def initialize(publisher, cover_state)
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  def can_be_archived?; end
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
 end
--- catalog_classes/lable.rb
+++ catalog_classes/lable.rb
@@ -1,8 +1,16 @@
 class Label
-  def initialize(title, color, _items)
+  attr_reader :items, :id
+  attr_accessor :title, :color
+
+  def initialize(title, color)
     @title = title
     @color = color
+    @id = rand(1...500)
+    @items []
   end
 
-  def add_item(item); end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self
+  end
 end
