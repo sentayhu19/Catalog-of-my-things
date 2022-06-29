541e4264e7a3a3690d694642a75f64dfa37bb176
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-29 18:13:22 -0400

    Create class to handle json files

---
+++ json/json_manipulation.rb
@@ -0,0 +1,19 @@
+require 'json'
+
+class Manipulation
+  def save_data(data, filname)
+    unless data.empty?
+      data_json = JSON.generate(data)
+      File.open("json/#{filname}", 'w') do |file|
+        file.puts data_json
+      end
+    end
+  end
+
+  def read_data(filname)
+    if File.exist?("json/#{filname}")
+      file = File.read("json/#{filname}")
+      data = JSON.parse(file, { symbolize_names: true })
+    end
+  end
+end
