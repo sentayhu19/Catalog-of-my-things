3cc7eee53797d82ab3c60450bb52ec3241c2c562
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-29 12:54:26 -0400

    Implement function add_item

--- catalog_classes/source.rb
+++ catalog_classes/source.rb
@@ -1,9 +1,15 @@
 class Source 
-  attr_accessor :items, :name
+  attr_accessor :name
+  attr_reader :id, :items
 
   def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
     @items = []
+  end
+
+  def add_item(item)
+    @items << (item)
+    item.source = self 
   end
 end
