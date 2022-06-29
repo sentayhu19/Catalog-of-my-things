cb9edde52d6c8387311a04e9bc7f17b8afef240b
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-29 18:14:17 -0400

    read movies and sources from json files

--- app.rb
+++ app.rb
@@ -1,18 +1,23 @@
 require './catalog_classes/movie'
 require './catalog_classes/item'
 require './catalog_classes/source'
+require './json/save_movie'
 
 class App
+  attr_reader :movies, :sources
+
   def initialize
-    @movies = []
-    @sources = []
+    readjson = Manipulation.new
+    @movies = readjson.read_data('movies.json') || []
+    @sources = readjson.read_data('sources.json') || []
   end
 
   def add_movie
     print 'Enter source: '
     input = gets.chomp
     source = Source.new(input)
-    @sources << source unless @sources.include?(source)
+    source_obj = { id: source.id, name: source.name }
+    @sources << source_obj unless @sources.include?(source_obj)
 
     print 'Author: '
     author = gets.chomp
@@ -23,17 +28,18 @@ class App
     print 'Is silent? [Y/N]: '
     silent = gets
     movie = Movie.new(author, source, label, publish_date, silent)
-    @movies << movie unless @movies.include?(movie)
+    movie_obj = { id: movie.id, author: movie.author, label: movie.label, publish_date: movie.publish_date }
+    @movies << movie_obj unless @movies.include?(movie_obj)
     puts 'Movie successfully created'
   end
 
   def list_all_sources
-    @sources.map { |source| p "ID: #{source.id} Name: #{source.name}" }
+    @sources.map { |source| p "ID: #{source[:id]} Name: #{source[:name]}" }
   end
 
   def list_all_movies
     @movies.map do |movie|
-      p "ID: #{movie.id} Author: #{movie.author} Label: #{movie.label} Publish date: #{movie.publish_date}"
+      p "ID: #{movie[:id]} Author: #{movie[:author]} Label: #{movie[:label]} Publish date: #{movie[:publish_date]}"
     end
   end
 
