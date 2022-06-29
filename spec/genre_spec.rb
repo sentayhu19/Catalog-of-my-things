5512dc3b3e7dd40860b445bba4cf3625caabeda4
moise <moisemlg90@gmail.com>
2022-06-29 20:45:46 +0200

    Create a spec folder and its files

---
+++ spec/genre_spec.rb
@@ -0,0 +1,9 @@
+require_relative './catalog_classes/genre'
+require_relative './catalog_classes/item'
+
+describe do
+    context "" do
+        it "" do
+        end
+    end
+end
---
+++ spec/music_album_spec.rb
@@ -0,0 +1,2 @@
+require_relative './catalog_classes/music_album'
+require_relative './catalog_classes/item'
