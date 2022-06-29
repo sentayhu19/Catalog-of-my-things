99810a5790cb9255abd4a55e74493a28d15927de
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-29 18:37:19 -0400

    correct linter errors

--- json/json_manipulation.rb
+++ json/json_manipulation.rb
@@ -2,18 +2,18 @@ require 'json'
 
 class Manipulation
   def save_data(data, filname)
-    unless data.empty?
-      data_json = JSON.generate(data)
-      File.open("json/#{filname}", 'w') do |file|
-        file.puts data_json
-      end
+    return if data.empty?
+
+    data_json = JSON.generate(data)
+    File.open("json/#{filname}", 'w') do |file|
+      file.puts data_json
     end
   end
 
   def read_data(filname)
-    if File.exist?("json/#{filname}")
-      file = File.read("json/#{filname}")
-      data = JSON.parse(file, { symbolize_names: true })
-    end
+    return unless File.exist?("json/#{filname}")
+
+    file = File.read("json/#{filname}")
+    JSON.parse(file, { symbolize_names: true })
   end
 end
