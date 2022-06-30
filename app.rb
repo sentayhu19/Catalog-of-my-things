61f7df0432051280d2fca235581adb3300da1bb4
unknown <didiersangwa@gmail.com>
2022-06-30 17:54:37 +0200

    Add game section

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
-require_relative 'catalog_classes/item'
-require_relative 'catalog_classes/genre'
+require 'json'
+require_relative 'classes/author'
+require_relative 'classes/game'
+require_relative 'store/game_read'
+require_relative 'store/game_store'
+require_relative 'inputs/inputs'
+require_relative 'modules/author_module'
 
-gender = Genre.new('Movie')
-items1 = Item.new(gender, 'Mielw', 'betaward', 'YMCMB', 2)
-gender.add_item(items1)
+BOOKS_FILE = 'data/books.json'.freeze
+LABELS_FILE = 'data/labels.json'.freeze
+
+class App
+  include AuthorModule
+
+  def initialize
+    @games = read_games
+    @authors = []
+    add_authors(@authors)
+  end
+
+  def list_all_authors
+    @authors.each_with_index do |author, index|
+      puts "#{index + 1}) #{author.first_name} #{author.last_name}"
+    end
+  end
+
+  def add_game
+    multiplayer, last_played_at, publish_date = grap_game_data
+    puts 'Choose an author from the following list using the number'
+    list_all_authors
+    author_chosen = gets.chomp.to_i
+    new_game = Game.new(multiplayer, last_played_at, publish_date)
+    new_game.add_author(@authors[author_chosen - 1])
+    @games << new_game
+    puts 'The game has been added successfully!'
+    save_games(@games)
+  end
+
+  def list_all_games
+    @games.each_with_index do |game, index|
+      puts "
+        #{index + 1}) Last Played: #{game.last_played_at}
+        Multiplayer: #{game.multiplayer}
+        publish: #{game.publish_date}"
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,43 +1,47 @@
-require './app'
+require_relative 'app'
 
-def menu
-  puts 'WELCOME TO CATALOG OF THINGS'
-  puts ''
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all music albums'
-  puts '3 - List all movies'
-  puts '4 - List all games'
-  puts '5 - List all genres'
-  puts '6 - List all labels'
-  puts '7 - List all authors'
-  puts '8 - List all sources'
-  puts '9 - Add a book'
-  puts '10 - Add music album'
-  puts '11 - Add a movie'
-  puts '12 - Add a games'
-  puts '13 - Exit'
+def display_list
+  puts "****************************\n*       MY CATALOGUE       *\n****************************\n\n\n"
+  puts "Select an option:\n--------------------\n"
+  puts "
+
+
+    4. List of games
+    12. Add a game
+    13. Exit"
+end
+
+def menu_one(option, app)
+  case option
+  when 4 then app.list_all_games
+  end
+end
+
+def menu_two(option, app)
+  case option
+  when 12 then app.add_game
+  end
 end
 
 def main
-  menu
-  option = gets.chomp.to_i
-  select_option(option)
-end
-
-def select_option(option)
-  case option
-  when 1..12
-    puts 'On construction'
-    print "\e[2J\e[f"
-    main
-  when 13
-    puts 'Goodbye see you again'
-    exit(true)
-  else
-    puts 'Choose a correct option'
-    main
+  app = App.new
+  option = ''
+  loop do
+    display_list
+    option = gets.chomp.to_i
+    if option.positive? && option < 7
+      menu_one(option, app)
+    elsif option >= 7 && option < 13
+      menu_two(option, app)
+    elsif option == 13
+      puts ' '
+      puts "\n\n\n********************\n*     Goodbye!     *\n********************\n"
+      exit(true)
+    else
+      puts "Invalid option, please try again\n--------------------\n\n\n"
+    end
   end
+ 
 end
 
 main
