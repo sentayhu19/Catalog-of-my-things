a6e20feab0aa22a447d092f8f31b5490afe955cb
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-30 11:59:12 -0400

    Created Unit testing for method can_be_archive

---
+++ catalog_classes/spec/movie_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../movie'
+
+describe Movie do
+  before do
+    source = double('Source')
+    items = []
+    allow(source).to receive(:name) {'From a friend'}
+    allow(source).to receive(:items) {items}
+    @movie = Movie.new 'Daniel', source, 'LABEL', '12/11/2010', false
+  end
+
+  it 'The method can_be_archived should return true' do
+    can_be_archived = @movie.can_be_archived?
+    expect(can_be_archived).to be true
+  end
+end
