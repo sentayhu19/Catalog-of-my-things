0bc21f2a4cad5a21c0813da29511a1b3733020bc
Masoudi Parrot <masoudi@localhost.localdomain>
2022-07-01 13:05:30 -0400

    update

--- catalog_classes/spec/movie_spec.rb
+++ catalog_classes/spec/movie_spec.rb
@@ -6,11 +6,11 @@ describe Movie do
     items = []
     allow(source).to receive(:name) { 'From a friend' }
     allow(source).to receive(:items) { items }
-    @movie = Movie.new 'Daniel', source, 'LABEL', '12/11/2010', false
+    @movie = Movie.new 'Daniel', 'LABEL', '12/11/2019', true
   end
 
   it 'The method can_be_archived should return true' do
     can_be_archived = @movie.can_be_archived?
-    expect(can_be_archived).to be true
+    expect(can_be_archived).to be false
   end
 end
