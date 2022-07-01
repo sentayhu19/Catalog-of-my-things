877950964d83ec7aee43482221546f4b2fd8af81
unknown <didiersangwa@gmail.com>
2022-07-01 12:32:34 +0200

    Refine group codes

--- app.rb
+++ app.rb
@@ -1,168 +1,165 @@
-require './catalog_classes/movie'
-require './catalog_classes/item'
-require './catalog_classes/source'
-require './json/json_manipulation'
-require './catalog_classes/music_album'
-require './catalog_classes/genre'
 require 'json'
+require_relative './classes/genre'
+require_relative './modules/list_genre'
+require_relative './classes/albums'
+require_relative './modules/list_album'
+require_relative 'classes/author'
+require_relative 'classes/game'
+require_relative 'classes/book'
+require_relative 'classes/label'
+require_relative 'modules/author_module'
+require_relative 'store/game_read'
+require_relative 'store/game_store'
+require_relative 'modules/book_module'
+require_relative 'modules/label_module'
+require_relative 'inputs/inputs'
+require_relative 'inputs/new_book_input'
+require_relative 'store/store_books'
+require_relative 'store/store_labels'
+require_relative 'store/load_books'
+require_relative 'store/load_labels'
+
+BOOKS_FILE = 'data/books.json'.freeze
+LABELS_FILE = 'data/labels.json'.freeze
 
 class App
-  attr_reader :movies, :sources
+  include AuthorModule
+  include BookModule
+  include LabelModule
+  include GenreList
+  include ManageAlbums
 
   def initialize
-    readjson = Manipulation.new
-    @movies = readjson.read_data('movies.json') || []
-    @sources = readjson.read_data('sources.json') || []
+    @labels = grab_labels
+    @books = grab_books
+    @music_albums = load_albums
+    @games = read_games
+    @genres = load_genres
+    @movies = []
+    @sources = []
+    @authors = []
+    save_genres
+    add_authors(@authors)
+    add_default_books(@books) unless @books.length.positive?
+    add_default_labels(@labels) unless @labels.length.positive?
   end
 
-  def add_movie
-    print 'Enter source: '
-    input = gets.chomp
-    source = Source.new(input)
-    source_obj = { id: source.id, name: source.name }
-    @sources << source_obj unless @sources.include?(source_obj)
+  def list_all_genres
+    puts "\n\n\nGenres:\n------------"
+    index = 1
+    @genres.each do |genre|
+      puts "#{index}. #{genre['name']}"
+      index += 1
+    end
+    puts "\n--------------------\n\n\n"
+  end
 
-    print 'Author: '
-    author = gets.chomp
-    print 'Label: '
-    label = gets.chomp
-    print 'Publish date in format d/mm/YYYY: '
+  def list_all_authors
+    @authors.each_with_index do |author, index|
+      puts "#{index + 1}) #{author.first_name} #{author.last_name}"
+    end
+  end
+
+  def list_all_music_albums
+    puts "\n\n\nAlbums:\n------------"
+    @music_albums.each do |album|
+      puts "
+      id: #{album['id']}
+      publish_date: (#{album['publish_date']})
+      Archived: #{album['archived']}
+      On spotify: #{album['on_spotify']}"
+    end
+    puts "\n--------------------\n\n\n"
+  end
+
+  def add_game
+    multiplayer, last_played_at, publish_date = grap_game_data
+    puts 'Choose an author from the following list using the number'
+    list_all_authors
+    author_chosen = gets.chomp.to_i
+    new_game = Game.new(multiplayer, last_played_at, publish_date)
+    new_game.add_author(@authors[author_chosen - 1])
+    @games << new_game
+    puts 'The game has been added successfully!'
+    save_games(@games)
+  end
+
+  def list_all_games
+    @games.each_with_index do |game, index|
+      puts "
+        #{index + 1}) Last Played: #{game.last_played_at}
+        Multiplayer: #{game.multiplayer}
+        publish: #{game.publish_date}"
+    end
+  end
+
+  def list_all_books
+    puts "\n\n\nBooks:\n------------"
+    @books.each_with_index do |book, index|
+      puts "
+      [#{index + 1}]
+      Publisher: #{book.publisher},
+      Cover State: #{book.cover_state}"
+    end
+  end
+
+  def list_all_labels
+    puts "\n\n\nLabels:\n------------"
+    @labels.each_with_index do |label, index|
+      puts "[#{index + 1}] Title: #{label.title}, Color: #{label.color} ID: #{label.id}"
+    end
+  end
+
+  def add_music_album
+    index = 1
+    puts 'Select genre:'
+    @genres.each do |genre|
+      puts "#{index}. #{genre['name']}"
+      index += 1
+    end
+    genre_index = gets.chomp.to_i
+    puts 'Enter publication date of the album:'
     publish_date = gets.chomp
-    print 'Is silent? [Y/N]: '
-    silent = gets
-    movie = Movie.new(author, source, label, publish_date, silent)
-    movie_obj = { id: movie.id, author: movie.author, label: movie.label, publish_date: movie.publish_date }
-    @movies << movie_obj unless @movies.include?(movie_obj)
-    puts 'Movie successfully created'
+    puts 'Is the album archived? (y/n)'
+    archived = gets.chomp == 'y'
+    puts 'Is the album on Spotify? (y/n)'
+    on_spotify = gets.chomp == 'y'
+    new_album = MusicAlbum.new(publish_date, archived, on_spotify)
+    @music_albums << { 'publish_date' => publish_date, 'archived' => archived, 'on_spotify' => on_spotify,
+                       'id' => new_album.id, 'genre' => @genres[genre_index - 1]['name'] }
+    save_music_albums
+    puts "\n\n\nAlbum added!\n------------\n\n\n"
   end
 
-  def list_all_sources
-    @sources.map { |source| p "ID: #{source[:id]} Name: #{source[:name]}" }
+  def add_book
+    new_book_input
+  end
+
+  def save_books
+    store_books
+  end
+
+  def save_labels
+    store_labels
+  end
+
+  def grab_labels
+    load_labels
+  end
+
+  def grab_books
+    load_books
   end
 
   def list_all_movies
-    @movies.map do |movie|
-      p "ID: #{movie[:id]} Author: #{movie[:author]} Label: #{movie[:label]} Publish date: #{movie[:publish_date]}"
-    end
+    puts 'Build pending... not currently available.'
   end
 
-  def redirection(option)
-    case option
-    when 3
-      list_all_movies
-    when 8
-      list_all_sources
-    when 9
-      add_movie
-    else
-      p 'Invalid input'
-
-    end
-  end
-end
-
-class MusicActions
-  attr_accessor :musics, :genres
-
-  def initialize
-    @musics = load_musics
-    @genres = load_genres
+  def list_all_sources
+    puts 'Build pending... not currently available.'
   end
 
-  def add_a_music
-    print 'Author: '
-    author = gets.chomp
-    puts 'Publish date (yyyy-mm-dd): '
-    publish_date = gets.chomp
-
-    puts 'Is it archived? [Y/N]: '
-    archived = gets.chomp.downcase
-
-    puts 'Is it on spotify? [Y/N]: '
-    on_spotify = gets.chomp.downcase
-
-    music = MusicAlbum.new(author, source, label, publish_date, archived, on_spotify)
-    @musics.push(music)
-
-    puts 'what is the name of the genre: '
-    name = gets.chomp
-    genre = Genre.new(name)
-
-    genre.add_item(music)
-    @genres.push(music.genre)
-    puts "\nMusicAlbum added successfully.\n"
-  end
-
-  def list_musics
-    if @musics.empty?
-      puts 'No music available yet!'
-    else
-      @musics.each do |music|
-        puts "
-              Music id: #{music.id}
-              Archived: #{music.archived == 'y' ? 'Yes' : 'No'}
-              On spotify : #{music.on_spotify}
-              Published on : #{music.publish_date}
-              "
-      end
-    end
-  end
-
-  def list_genres
-    if @genres.empty?
-      puts 'no genres available!'
-    else
-      @genres.each do |genre|
-        puts "
-              genre's id: #{genre.id}
-              genre's name: #{genre.name}
-            "
-      end
-    end
-  end
-
-  def load_musics
-    data = []
-    file = './data_JSON/musics.json'
-    if File.exist?(file)
-      JSON.parse(File.read(file)).each do |music|
-        data.push(MusicAlbum.new(music['publish_date'], music['archived'], music['on_spotify']))
-      end
-    else
-      File.write(file, [])
-    end
-    data
-  end
-
-  def load_genres
-    data = []
-    file = './data_JSON/genres.json'
-    if File.exist?(file)
-      JSON.parse(File.read(file)).each do |genre|
-        data.push(Genre.new(genre['name']))
-      end
-    else
-      File.write(file, [])
-    end
-    data
-  end
-
-  def save_musics
-    data = []
-    @musics.each do |music|
-      data.push({ id: music.id,
-                  archived: music.archived,
-                  on_spotify: music.on_spotify,
-                  publish_date: music.publish_date })
-    end
-    File.write('./data_JSON/musics.json', JSON.pretty_generate(data))
-  end
-
-  def save_genres
-    data = []
-    @genres.each do |genre|
-      data.push({ id: genre.id, name: genre.name })
-    end
-    File.write('./data_JSON/genres.json', JSON.pretty_generate(data))
+  def add_movie
+    puts 'Build pending... not currently available.'
   end
 end
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,35 +1,46 @@
-require_relative '../catalog_classes/genre'
-require_relative '../catalog_classes/item'
+require_relative '../classes/genre'
+require_relative '../modules/list_genre'
 
 describe Genre do
-  context 'it should work for the genre class' do
-    let(:genre) { Genre.new('Pop music') }
-    # @genre = Genre.new('')
-    it 'should return "@genre" as an instance variable of Genre class' do
-      expect(genre).to be_an_instance_of Genre
-    end
+  include GenreList
 
-    it 'should return attribute name of music genre' do
-      expect(genre.name).to eq 'Pop music'
-    end
+  before(:each) do
+    @genre = Genre.new('Rock')
+  end
 
-    it 'should check a typo error of the attribute name of music genre' do
-      expect(genre.name).not_to eq 'Pop musics'
-    end
+  it 'should be an instance of Genre' do
+    expect(@genre).to be_an_instance_of(Genre)
+  end
 
-    it 'should return an empty array as the default value of "@items"' do
-      expect(genre.items).to eq []
-    end
+  it 'should have a name' do
+    expect(@genre.name).to eq('Rock')
+  end
 
-    it 'should return the length of "@items"' do
-      expect(genre.items.length).to eq 0
-    end
+  it 'should have an id' do
+    expect(@genre.id).to be_instance_of(Integer)
+  end
 
-    it 'should add an item in items list' do
-      item = Item.new
-      genre.add_item(item)
-      expect(genre.items.length).to eq 1
-      expect(genre.items).to eq [item]
-    end
+  it 'should have an item' do
+    expect(@genre.item).to be_instance_of(Array)
+  end
+
+  it 'should create a json file with the genre name' do
+    save_genres
+    expect(File.exist?('./data/genre.json')).to eq(true)
+  end
+
+  it 'Should have data stored in the json file' do
+    save_genres
+    expect(File.read('./data/genre.json')).to_not eq('')
+  end
+
+  it 'should return an array of genres' do
+    expect(load_genres).to be_instance_of(Array)
+  end
+
+  it 'should be able to add an item' do
+    @item = Item.new('12/02/2018', false)
+    @genre.add_item(@item)
+    expect(@genre.item).to include(@item)
   end
 end
